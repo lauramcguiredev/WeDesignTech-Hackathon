b94125399fd81ed31e2940a3474d31b55c2d1bb8
Laura McGuire <laura.mcguire.dev@gmail.com>
2021-02-13 16:34:27 -0500

    Updated UI for Sign Up and Login Updated theme colours

--- app/src/main/java/com/example/axolotl/LoginActivity.java
+++ app/src/main/java/com/example/axolotl/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.axolotl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button signUpButton = findViewById(R.id.createAccountButton);
+        final Button loginButton = findViewById(R.id.loginButton);
+        final EditText userField = findViewById(R.id.emailField);
+        final EditText passField = findViewById(R.id.passField);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+            }
+        });
     }
 }
