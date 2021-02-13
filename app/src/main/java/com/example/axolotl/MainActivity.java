76122ca63b2e62cbd00e8b2b6d828d7d41c172c9
Laura McGuire <laura.mcguire.dev@gmail.com>
2021-02-13 17:11:40 -0500

    Added logout button to Main

--- app/src/main/java/com/example/axolotl/MainActivity.java
+++ app/src/main/java/com/example/axolotl/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.axolotl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcome = findViewById(R.id.welcome);
+        final Button logout = findViewById(R.id.logoutButton);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+        welcome.setText(user.getEmail());
     }
 }
